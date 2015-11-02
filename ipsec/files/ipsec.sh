57f983f17a1d4e38d2b51738d0e10d3149097fb2
Volodymyr Lapada <Volodymyr.Lapada@tomtom.com>
2015-11-02 22:51:09 +0100

    ipsec.sh changed

--- ipsec/files/ipsec.sh
+++ ipsec/files/ipsec.sh
@@ -1,8 +1,11 @@
-echo "net.ipv4.ip_forward = 1" |  /usr/bin/tee -a /etc/sysctl.conf
-echo "net.ipv4.conf.all.accept_redirects = 0" |  /usr/bin/tee -a /etc/sysctl.conf
-echo "net.ipv4.conf.all.send_redirects = 0" |  /usr/bin/tee -a /etc/sysctl.conf
-for vpn in /proc/sys/net/ipv4/conf/*; do echo 0 > $vpn/accept_redirects; echo 0 > $vpn/send_redirects; done
-/sbin/sysctl -p
-if [ $? -eq 255 ];then 
-	exit 0 
+aaa=$(cat /etc/sysctl.conf |grep "net.ipv4.ip_forward = 1")
+if [ $? -ne 0 ];then
+ echo "net.ipv4.ip_forward = 1" |  /usr/bin/tee -a /etc/sysctl.conf
+ echo "net.ipv4.conf.all.accept_redirects = 0" |  /usr/bin/tee -a /etc/sysctl.conf
+ echo "net.ipv4.conf.all.send_redirects = 0" |  /usr/bin/tee -a /etc/sysctl.conf
+ for vpn in /proc/sys/net/ipv4/conf/*; do echo 0 > $vpn/accept_redirects; echo 0 > $vpn/send_redirects; done
+ /sbin/sysctl -p
+ if [ $? -eq 255 ];then
+         exit 0
+ fi
 fi
