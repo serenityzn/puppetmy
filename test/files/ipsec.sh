613b3934b818159d3f39617df0cf4960fed811c0
Volodymyr Lapada <Volodymyr.Lapada@tomtom.com>
2015-11-02 21:39:22 +0100

    ipsec.sh fix

--- test/files/ipsec.sh
+++ test/files/ipsec.sh
@@ -3,3 +3,6 @@ echo "net.ipv4.conf.all.accept_redirects = 0" |  /usr/bin/tee -a /etc/sysctl.con
 echo "net.ipv4.conf.all.send_redirects = 0" |  /usr/bin/tee -a /etc/sysctl.conf
 for vpn in /proc/sys/net/ipv4/conf/*; do echo 0 > $vpn/accept_redirects; echo 0 > $vpn/send_redirects; done
 /sbin/sysctl -p
+if [ $? -eq 255 ];then 
+	exit 0 
+fi
