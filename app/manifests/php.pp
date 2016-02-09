256162621ef0883b6dc91dab3b7342ebaedfe089
root <you@example.com>
2016-02-09 21:45:31 +0000

    added phpunit to php.pp

--- app/manifests/php.pp
+++ app/manifests/php.pp
@@ -1,6 +1,6 @@
 class app::php()
 {
- package { [ 'php5', 'php5-mysql', 'php5-tidy', 'php5-gd', 'php-apc', 'php-pear', 'php5-apcu', 'php5-curl', 'php5-dev', 'php5-json', 'php5-mcrypt', 'php5-readline', 'php5-redis', 'php5-xdebug', 'pkg-php-tools' ]:
+ package { [ 'php5', 'php5-mysql', 'php5-tidy', 'php5-gd', 'php-apc', 'php-pear', 'php5-apcu', 'php5-curl', 'php5-dev', 'php5-json', 'php5-mcrypt', 'php5-readline', 'php5-redis', 'php5-xdebug', 'pkg-php-tools', 'phpunit' ]:
   ensure => 'installed',
  } ->
  file { [ '/etc/php5/apache2/php.ini', '/etc/php5/cli/php.ini' ]:
